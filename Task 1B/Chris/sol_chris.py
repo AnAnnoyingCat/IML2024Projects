a120ba786c0ac3b0fbb58705355f52f6665cb2cb
cwernkeDesktop <cwernke@student.ethz.ch>
2024-03-13 20:12:46 +0100

    Many things

--- Task 1B/Chris/sol_chris.py
+++ Task 1B/Chris/sol_chris.py
@@ -3,10 +3,11 @@
 # First, we import necessary libraries:
 import numpy as np
 import pandas as pd
-
+from matplotlib import pyplot as plt
 # Add any additional imports here (however, the task is solvable without using 
 # any additional imports)
-# import ...
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 def transform_data(X):
     """
@@ -48,6 +49,7 @@ def transform_data(X):
     X_transformed[:, 20] = np.full(700, 1)
 
     #print(X_transformed)
+    np.savetxt("Task 1B\\Chris\\transformed.csv", X_transformed, fmt="%.12f", delimiter=',')
     assert X_transformed.shape == (700, 21)
     return X_transformed
 
@@ -66,12 +68,77 @@ def fit(X, y):
     ----------
     w: array of floats: dim = (21,), optimal parameters of linear regression
     """
+
     w = np.zeros((21,))
     X_transformed = transform_data(X)
-    # TODO: Enter your code here
+    X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state=13)
+
+
+    ITERATIONS = [5, 10, 15, 20, 100, 1000, 3500, 6500, 10000, 20000, 50000, 100000, 200000, 500000]
+    LEARNING_RATES = [0.15, 0.125, 0.1, 0.09, 0.07, 0.05, 0.025]#, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00001, 0.000001
+    #ITERATIONS = [5, 10, 15, 20, 50, 100, 150,200, 300, 500, 700, 850, 1000]
+    #LEARNING_RATES = [0.15, 0.125, 0.1]
+
+    RMSE_mat = np.zeros((len(LEARNING_RATES), len(ITERATIONS)))
+    
+    currRate = 0
+    for rate in LEARNING_RATES:
+        currItr = 0
+        for itr in ITERATIONS:
+            print("attempting rate of: " + str(rate) + " at " + str(itr) + " iterations")
+            w = myLinearRegression(X_train, y_train, rate, itr)
+            y_pred = np.dot(X_test, w)  # find y_pred w/ help of X and w
+            RMSE_mat[currRate][currItr] = mean_squared_error(y_test, y_pred, squared=False)
+            print("done")
+            currItr += 1
+        currRate += 1
+    saveManyPlots(RMSE_mat, ITERATIONS, LEARNING_RATES)
+
+    w = myLinearRegression(X_train, y_train, 0.1, 200)
+    #savePlot(ITERATIONS, cost_list)
     assert w.shape == (21,)
     return w
 
+def myLinearRegression(X, y, learning_rate, iterations):
+    """
+    My attempt at linear regression. Explained and inspired by https://www.youtube.com/watch?v=sRh6w-tdtp0. 
+    """
+    m = y.size
+    weights = np.zeros((X.shape[1]))
+    #cost_list = []
+
+    for i in range (iterations):
+
+        y_pred = np.dot(X, weights)
+
+        cost = (1/(2*m)) * np.sum(np.square(y_pred - y))
+
+        d_weights = (1/m) * np.dot(X.T, (y_pred - y.T))
+        uhm = (y - y_pred)
+        weights = weights - learning_rate * d_weights
+        #cost_list.append(cost)
+
+        if (i % (iterations / 10) == 0):
+            print("Cost is: ", cost)
+    
+    return weights
+
+def savePlot(iterations, cost_list):
+    rng = np.arange(0, iterations)
+    plt.plot(rng, cost_list)
+    plt.title(("Development of costs over the course of " + str(iterations) + " iterations."))
+    plt.yscale("log")
+    plt.savefig("Task 1B\\Chris\\costfunction.png")
+
+def saveManyPlots(RMSE_mat, ITERATIONS, LEARNING_RATES):
+    for r in range(len(LEARNING_RATES)):
+        plt.clf()
+        plt.yscale("log")
+        plt.plot(ITERATIONS, RMSE_mat[r,:])
+        plt.title(("Change of RMSE with learning rate " + str(LEARNING_RATES[r])))
+        #plt.savefig("Task 1B\\Chris\\LR-Plots\\rate" + str(r) + ".png")
+        name = "Task 1B\\Chris\\LR-Plots\\rate" + str(r) + ".png"
+        plt.savefig(name)
 
 # Main function. You don't have to change this
 if __name__ == "__main__":
@@ -80,7 +147,7 @@ if __name__ == "__main__":
     y = data["y"].to_numpy()
     data = data.drop(columns=["Id", "y"])
     # print a few data samples
-    print(data.head())
+    # print(data.head())
 
     X = data.to_numpy()
     # The function retrieving optimal LR parameters
