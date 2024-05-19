8275382ed0a27aaf748e41a5878dad28bb23f7f3
cwernkeDesktop <cwernke@student.ethz.ch>
2024-05-19 13:30:50 +0200

    first test

--- Task 4/Chris/template_solution.py
+++ Task 4/Chris/template_solution.py
@@ -23,25 +23,6 @@ BATCH_SIZE = 64
 NUM_EPOCHS = 10
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-class MyModule(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(64, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 1)
-        self.dropout = nn.Dropout(0.5)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc3(x))
-        x = self.dropout(x)
-        x = self.fc4(x)
-        return x
-
 class TrainReviewDataset(Dataset):
     def __init__(self, data_frame, tokenizer, max_length = 128):
         self.data = data_frame.values 
@@ -95,7 +76,7 @@ class TestReviewDataset(Dataset):
 
         return {
             'input_ids': encoded_dict['input_ids'].squeeze(0),
-            'attention_mask': encoded_dict['attention_mask'].squeeze(0),
+            'attention_mask': encoded_dict['attention_mask'].squeeze(0)
         }
 
 class EmbeddedDataset(Dataset):
@@ -108,9 +89,36 @@ class EmbeddedDataset(Dataset):
 
     def __getitem__(self, idx):
         embedding = self.embeddings[idx]
-        score = self.embeddings[idx]
+        score = self.scores[idx]
         return embedding, score
-        
+
+class EmbeddedDatasetTest(Dataset):
+    def __init__(self, embeddings):
+        self.embeddings = embeddings
+
+    def __len__(self):
+        return len(self.embeddings)
+
+    def __getitem__(self, idx):
+        embedding = self.embeddings[idx]
+        return embedding
+
+class MyModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(768, 64)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
+
 def makeEmbeddings():
     train_val = pd.read_csv("Task 4/Data/train.csv")
     test_val = pd.read_csv("Task 4/Data/test_no_score.csv")
@@ -120,10 +128,10 @@ def makeEmbeddings():
 
     train_loader = DataLoader(dataset=train_dataset,
                             batch_size=BATCH_SIZE,
-                            shuffle=False, num_workers=4, pin_memory=True)
+                            shuffle=False, num_workers=1, pin_memory=True)
     test_loader = DataLoader(dataset=test_dataset,
                             batch_size=BATCH_SIZE,
-                            shuffle=False, num_workers=4, pin_memory=True)
+                            shuffle=False, num_workers=1, pin_memory=True)
     #the embeddings get generated in the train/testloader directly.
     print("starting the embedding process")
     model = DistilBertModel.from_pretrained("distilbert-base-uncased")
@@ -134,7 +142,7 @@ def makeEmbeddings():
     embeddings_list_cls = []
     scores_list = []
     
-    for batch in tqdm(train_loader, desc="Processing batches"):
+    for batch in tqdm(test_loader, desc="Processing batches"):
         # move the stuff to GPU
         input_ids = batch['input_ids'].to(DEVICE)
         attention_mask = batch['attention_mask'].to(DEVICE)
@@ -147,39 +155,42 @@ def makeEmbeddings():
         last_hidden_states = torch.tensor(outputs.last_hidden_state.cpu())
 
         #get mean, max and cls
-        #mean_embedding = torch.mean(last_hidden_states, dim=1)
+        mean_embedding = torch.mean(last_hidden_states, dim=1)
         #max_embedding, _ = torch.max(last_hidden_states, dim=1)
-        cls_embedding = last_hidden_states[:, 0, :]
+        #cls_embedding = last_hidden_states[:, 0, :]
 
-        scores = batch['score'].tolist()
+        #scores = batch['score'].tolist()
 
-        #embeddings_list_mean.append(mean_embedding)
+        embeddings_list_mean.append(mean_embedding)
         #embeddings_list_max.append(max_embedding)
-        embeddings_list_cls.append(cls_embedding)
-        scores_list.extend(scores)
+        #embeddings_list_cls.append(cls_embedding)
+        #scores_list.extend(scores)
 
-    last_tensor = embeddings_list_cls[-1]
+    last_tensor = embeddings_list_mean[-1]
 
     #pad last tensor
     padding_size = (64 - last_tensor.size(0), last_tensor.size(1))
     padded_last_tensor = torch.cat([last_tensor, torch.zeros(*padding_size)], dim=0)
-    embeddings_list_cls[-1] = padded_last_tensor
-    concatenated_embeddings_cls = torch.cat(embeddings_list_cls, dim=0)
+    embeddings_list_mean[-1] = padded_last_tensor
+    concatenated_embeddings_mean = torch.cat(embeddings_list_mean, dim=0)
     #remove padding
-    concatenated_embeddings_cls = concatenated_embeddings_cls[:(-1*padding_size[0])]
+    concatenated_embeddings_mean = concatenated_embeddings_mean[:(-1*padding_size[0])]
 
-    scores = np.array(scores_list)
+    #scores = np.array(scores_list)
 
     #np.savez("distilibertEmbeddings_mean.npy", embeddings=concatenated_embeddings_mean, scores=scores)
     #np.savez("distilibertEmbeddings_max.npy", embeddings=concatenated_embeddings_max, scores=scores)
-    np.savez("distilibertEmbeddings_cls.npy", embeddings=concatenated_embeddings_cls, scores=scores)
+    #np.savez("distilibertEmbeddings_cls.npy", embeddings=concatenated_embeddings_cls, scores=scores)
+    np.savez("distilibertEmbeddings_mean_test.npy", embeddings=concatenated_embeddings_mean)
+    #np.savez("distilibertEmbeddings_max_test.npy", embeddings=concatenated_embeddings_max)
+    #np.savez("distilbertEmbeddings_cls_test.npy", embeddings=concatenated_embeddings_cls)
 
     print("Saved embeddings and scores")
 
 
 if __name__ == '__main__':
-    file_path = "Task 4\Chris\distilbertEmbeddings_mean.npy.npz"
-    data = np.load(file_path)
+    train_path = "Task 4\Chris\distilbertEmbeddings_cls.npy.npz"
+    data = np.load(train_path)
     embeddings = data['embeddings']
     scores = data['scores']
 
@@ -190,7 +201,7 @@ if __name__ == '__main__':
 
     train_dataset, val_dataset = data_utils.random_split(embedded_dataset, [train_size, val_size])
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
     val_loader = DataLoader(dataset=val_dataset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
 
     model = MyModule()
@@ -198,11 +209,12 @@ if __name__ == '__main__':
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
-    model.train()
+    #=================TRAIN========================
     model.to(DEVICE)
+    print("beginning training")
     for epoch in range(NUM_EPOCHS):
-
         #train it
+        model.train()
         epoch_loss = 0 
         for X_train, y_train in train_loader:
             X_train, y_train = X_train.to(DEVICE), y_train.to(DEVICE)
@@ -225,17 +237,21 @@ if __name__ == '__main__':
         val_loss /= len(val_loader)
         print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
 
+    test_path = "Task 4\Chris\distilbertEmbeddings_cls_test.npy.npz"
+    data_test = np.load(test_path)
+    embeddings_test = data_test['embeddings']
 
-"""
+    #=================TEST======================
+    embedded_dataset_test = EmbeddedDatasetTest(embeddings_test)
+    test_loader = DataLoader(dataset=embedded_dataset_test, batch_size=64, shuffle=False, num_workers=4, pin_memory=True)
     model.eval()
     with torch.no_grad():
         results = []
-        for batch in tqdm(test_loader, total=len(test_loader)):
-            batch = batch.to(DEVICE)
-
-            # TODO: Set up evaluation loop
-
-        with open("result.txt", "w") as f:
-            for val in np.concatenate(results):
-                f.write(f"{val}\n")
-"""
+        for X in tqdm(test_loader, total=len(test_loader)):
+            X = X.to(DEVICE)
+            test_output = model(X)
+            results.append(test_output.cpu().numpy())
+        with open("Task 4\Chris\result.txt", "w") as f:
+            for batch_results in results:
+                for val in batch_results:
+                    f.write(f"{val}\n")
